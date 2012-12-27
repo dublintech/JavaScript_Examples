66389d413158e028f2e2d5574dc7239bd34ea397
Alex Staveley <breako@gmail.com>
2012-12-27 20:40:31 +0000

    modified:   css/jquiz.css 	new file:   images/ajax-loader.gif

--- jsquiz/js/jQuiz.js
+++ jsquiz/js/jQuiz.js
@@ -26,15 +26,22 @@ var quizModule = function (questionAnswers, divMarker) {
 			} else {
 			   returnString.push('<div class="questionContainer hide radius">');
 			}
-			
-			returnString.push('<div class="question"><b><span>' + val.question + '</span></b></div>');
+			returnString.push('<div class="question"><b>Question '  + (i+1) + ': ' + '</b><b><span>' + val.question + '</span></b></div>');
 
 			returnString.push('<div class="answers">');  // answers div begin
 			returnString.push('<ul>');                   // list begin
 			
 			var textForAnswers = val.answers;
 			$.each(textForAnswers, function(j, answer) {
-			    returnString.push('<li class="answeritem"><label><input type="radio" name="q' + i + '" id="q' + i + '-' + j + '"/><span>' +  answer + '</span></label></li>');    // Add each answer
+			    // get answer.
+				var actualAnswer = val.correct_answer;
+				if (val.correct_answer.split(',').length === 1) {
+					// use check boxes
+					returnString.push('<li class="answeritem"><label><input type="radio" name="q' + i + '" id="q' + i + '-' + j + '"/><span>' +  answer + '</span></label></li>');    // Add each answer
+				} else {
+					// else use radio buttons.
+					returnString.push('<li class="answeritem"><label><input type="checkbox" name="q' + i + '" id="q' + i + '-' + j + '"/><span>' +  answer + '</span></label></li>');    // Add each answer
+				}
 			}); // end answer iteration.
 			
 			returnString.push('</ul>');							// list end
@@ -61,7 +68,7 @@ var quizModule = function (questionAnswers, divMarker) {
 			returnString.push('</div>');                        // end panel div
 
 	   });
-	   returnString.push('<div id="txtStatusBar">Status Bar</div>');
+	   returnString.push('<div id="txtStatusBar">Progess Bar</div>');
 	   returnString.push('<div id="progressKeeper" class="radius">');
 	   returnString.push('<div id="progress"></div>');
 	   returnString.push('</div>');
@@ -72,15 +79,17 @@ var quizModule = function (questionAnswers, divMarker) {
 	
 	function addEventListenets(quizQuestions){
 		$('.btnNext').click(function(){
-			if ($('input[type=radio]:checked:visible').length == 0) {
-						
+			if (($('input[type=radio]:checked:visible').length == 0) && ($('input[type=checkbox]:checked:visible').length === 0)) {
 				return false;
 			}
 			$(this).parents('.questionContainer').fadeOut(500, function(){
 				$(this).next().fadeIn(500)
 			});
 			var el = $('#progress');
-			el.width(el.width() + ($('#progressKeeper').width() / quizQuestions.questions.length) + 'px');
+			var totalIncreaseAmount = ($('#progressKeeper').width() / quizQuestions.questions.length);
+			//el.width((el.width() + totalIncreaseAmount) + 'px');
+			var newWidth = el.width() + totalIncreaseAmount;
+			el.animate({width:newWidth + 'px'}, 500);
 		});
 		$('.btnPrev').click(function(){
 			$(this).parents('.questionContainer').fadeOut(500, function(){
@@ -90,43 +99,100 @@ var quizModule = function (questionAnswers, divMarker) {
 			el.width(el.width() - ($('#progressKeeper').width()  / quizQuestions.questions.length) + 'px');
 		})
 		$('.btnShowResult').click(function(){
-			var arr = $('input[type=radio]:checked');
-			var userAnswers = [];
-			for (var i = 0, ii = arr.length; i < ii; i++) {
-				userAnswers.push(arr[i].getAttribute('id'))
-			}
+			var el = $('#progress');
+			el.width($('#progressKeeper').width() + 'px');
+			
+			// gather user Answers.
+			var $arrRadio = $('input[type=radio]:checked');
+			var $arrCheck = $('input[type=checkbox]:checked');
+			var userAnswersMap = gatherUserAnswers($arrRadio, $arrCheck);	
+			
             // progress update
 			// remove it.
 			$('#progressKeeper').fadeOut(500);
 			$('#txtStatusBar').fadeOut(500);
-			
-			displayResults(userAnswers, quizQuestions);
+			$('.btnNext').fadeOut(500);
+			$('.btnPrev').fadeOut(500);
+			$('.btnShowResult').fadeOut(500, displayResults(userAnswersMap, quizQuestions));
 		})
     }
 	
-	function displayResults(userAnswers, quizQuestions) {
+	function gatherUserAnswers(arrRadio, arrCheck) {
+		var totalArr = arrRadio.add(arrCheck);
+		var userAnswersMap = {};  // Collates check boxes for the same question.
+		
+		for (var i = 0, ii = totalArr.length; i< ii; i++) {
+			// get id, will be of form: input#q0-0, input#q1-0
+			var id = totalArr[i].getAttribute('id');  
+			var questionPart = id.split('-')[0];
+			var questionNumber = questionPart.split('q')[1];
+			var userAnswer = id.split('-')[1];
+			if (userAnswersMap[questionNumber]) {
+				var currentAnswer = userAnswersMap[questionNumber];
+				if (currentAnswer < userAnswer) {
+					userAnswersMap[questionNumber] = currentAnswer + "," + userAnswer;
+				} else {
+					userAnswersMap[questionNumber] = userAnswer + "," + currentAnswer;
+				}
+			} else {
+				userAnswersMap[questionNumber] = userAnswer;
+			}
+		}
+		
+		return userAnswersMap;
+	}
+	
+	function displayResults(userAnswersMap, quizQuestions) {
 		// Now check answers.
-		var results = checkAnswers(userAnswers, quizQuestions);
+		var results = checkAnswers(userAnswersMap, quizQuestions);
 		var resultSet = '';
 		var trueCount = 0;
 			
 		resultSet += '<table id="resultsTable">';
 		resultSet += '<caption>Quiz Results</caption>'
 		resultSet += '<tr><th></th><th>Question</th><th>Your answer</th><th>Correct answer</th><th>Pass / Fail</th></tr>';
-		for (var i = 0, ii = results.length; i < ii; i++){
+		for (var i = 0, ii = quizQuestions.questions.length; i < ii; i++){
 			if (results[i] == true) trueCount++;
 			var passFailCell = '';
 			if (results[i]) {
 				passFailCell = '<td>Pass</td>';   
 			} else {
-				passFailCell = '<td COLOR="Red">Fail</td>';
+				passFailCell = '<td CLASS="fail">Fail</td>';
 			}
 				
-			// for ( var j = 0, j < quizQuestions.questions[i].answers.length, j++) { 
-			// var answerTokenArray = quizQuestions.questions[i].answers[j].
-			var userAnswerIndex = userAnswers[i].split('-')[1];
-			var userAnswer = quizQuestions.questions[i].answers[userAnswerIndex];
-            var correctAnswer = quizQuestions.questions[i].answers[quizQuestions.questions[i].correct_answer];
+			var userAnswerIndexes = userAnswersMap[i].split(',');
+			var userAnswer = '';
+			
+			if (userAnswerIndexes.length > 1) {
+				userAnswer += '<UL class="resultlist">';
+				for ( var j = 0; j < userAnswerIndexes.length; j++) {
+					if ((j % 2) === 0) {
+						userAnswer += '<LI class="altli">' + quizQuestions.questions[i].answers[j] + '</LI>';
+					} else {
+						userAnswer += '<LI>' + quizQuestions.questions[i].answers[j] + '</LI>';
+					}
+				}
+				userAnswer += '</UL>';
+			} else {
+			    userAnswer += quizQuestions.questions[i].answers[j]
+			}
+			
+			var correctAnswerIndexes = quizQuestions.questions[i].correct_answer.split(',');
+			var correctAnswer = '';
+			if (correctAnswerIndexes.length > 1) {
+				correctAnswer += '<UL class="resultlist">';
+				for ( var j = 0; j < correctAnswerIndexes.length; j++) {
+					if ((j % 2) === 0) {
+						correctAnswer += '<LI class="altli">' + quizQuestions.questions[i].answers[j] + '</LI>';
+					} else {
+						correctAnswer += '<LI>' + quizQuestions.questions[i].answers[j] + '</LI>';
+					}
+				}
+				correctAnswer += '</UL>';
+			} else {
+			    correctAnswer += quizQuestions.questions[i].answers[j]
+			}
+ 
 			if ((i % 2) === 0) { 
 				resultSet += '<tr>';
 			} else {
@@ -138,20 +204,20 @@ var quizModule = function (questionAnswers, divMarker) {
 		}
 		resultSet += '</table>';
 		resultSet += '<div class="totalScore">Your total score is ' + trueCount * (100 / quizQuestions.questions.length) + '!</div>'
+		resultSet += '<div><I>Refresh page to try again</I></div>';
 		$('#resultKeeper').html(resultSet).show();
 	}
 	
-	function checkAnswers(userAnswers, quizQuestions) {
+	function checkAnswers(userAnswersMap, quizQuestions) {
 		var resultArr = [];
-		for (var i = 0, ii = userAnswers.length; i < ii; i++){ 
-			var userAnswerIndex = userAnswers[i].split('-')[1];;
+		for (var i = 0, ii = quizQuestions.questions.length; i < ii; i++){
+			var userAnswer = userAnswersMap[i];;
 			var questionInfo = quizQuestions.questions[i];
 			var correctAnswer = questionInfo['correct_answer'];
 			var success = false;
-			if (userAnswerIndex === correctAnswer) {
+			if (userAnswer === correctAnswer) {
 				success = true;
-			}
-			else {
+			} else {
 				success = false;
 			}
 			resultArr.push(success);
